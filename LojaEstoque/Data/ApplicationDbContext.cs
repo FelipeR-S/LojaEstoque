da1e34ef3cc7b239e9b9f5bd975df005132a1ced
Felipe Rodrigues Santos <feliperodrigues.dw@gmail.com>
2023-01-24 15:49:49 -0300

    Projeto inicializado
    Base para o projeto inserida

---
+++ LojaEstoque/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using LojaEstoque.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LojaEstoque.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Produto>();
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ LojaEstoque/Models/Modelo.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+using System.ComponentModel.DataAnnotations;
+
+namespace LojaEstoque.Models
+{
+    [DataContract]
+    public abstract class BaseModel
+    {
+        [DataMember]
+        [Key]
+        public int Id { get; set; }
+    }
+
+    public class Produto : BaseModel
+    {
+        public Produto()
+        {
+        }
+        public string Nome { get; set; } = string.Empty;
+
+    }
+}
